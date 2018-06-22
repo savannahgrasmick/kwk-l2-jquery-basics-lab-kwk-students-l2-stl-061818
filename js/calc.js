a42fc327be488daf8bb53ece1714460f64654c24
savannahgrasmick <grasmicksavannah@gmail.com>
2018-06-22 21:21:01 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -8,13 +8,14 @@ let num2 = $("#number2")
 function validate(operator, num1, num2) {
   
   if (operator == "+" || operator == "-" || operator == "*" || operator == "/") {
-    if (isNaN(num1) || num1 === "" || 
+    if (isNaN(num1) || num1 === "" || isNaN(num2) || num2 === "")
+
+      $("#result").text("Sorry,one of those is not a valid number!")
     
-    
-    
-    
-    
-  }
+      } else {
+        answer(operator, num1, num2); 
+      }
+}
   
   
 }
